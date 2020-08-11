9f3e8de16edc400e4dca8e5014e6aa4623b53fa6
TheTrabin <beyondtears@gmail.com>
2020-08-10 17:30:21 -0700

    Login functions from CRA

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -22,7 +22,7 @@ class Login extends React.Component {
   login = e => {
     e.preventDefault();
     axiosWithAuth()
-      .post("/login", this.state.credentials)
+      .post("/auth/login", this.state.credentials)
       .then(res => {
         localStorage.setItem("token", res.data.payload);
         this.props.history.push("/protected");
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "localhost:5000/api/",
+    baseURL: "http://localhost:5000/api/",
     headers: {
       headers: {
         'Authorization': "Bearer" + token
